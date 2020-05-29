c0d1f526f2330ead06ecada1046e0e049bd65d9f
deployAt <nzbiegien@gmail.com>
2020-05-29 09:01:17 +0100

    feat/test: tdd Block - genesis

--- test/block.test.js
+++ test/block.test.js
@@ -1,4 +1,5 @@
 const Block = require('../src/block')
+const { GENESIS_DATA } = require('../src/config')
 
 describe('Block', () => {
   const timestamp = 'time'
@@ -6,12 +7,24 @@ describe('Block', () => {
   const hash = 'hash'
   const data = ['a', 'b']
 
-  const block = new Block ({ timestamp, lastHash, hash, data })
+  const block = new Block({ timestamp, lastHash, hash, data })
 
   it('has timestamp, lastHash, hash and data', () => {
-    expect(block.timestamp).toEqual(timestamp)
+    expect(block.timestamp).toEqual(timestamp) //?
     expect(block.lastHash).toEqual(lastHash)
     expect(block.hash).toEqual(hash)
     expect(block.data).toEqual(data)
+  })
+
+  describe('genesis()', () => {
+    const genesisBlock = Block.genesis()
+
+    it('return a Block instance', () => {
+      expect(genesisBlock instanceof Block).toBe(true)
+    })
+
+    it('returns the gensis data', () => {
+      expect(genesisBlock).toEqual(GENESIS_DATA)
+    })
   })
 })
