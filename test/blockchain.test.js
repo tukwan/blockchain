1b76fb87ca4e7e6ca214952873abd3f4fc7076cd
deployAt <nzbiegien@gmail.com>
2020-05-30 08:12:38 +0100

    test: tdd Blockchain - isValidChain

--- test/blockchain.test.js
+++ test/blockchain.test.js
@@ -2,7 +2,7 @@ const Blockchain = require('../src/blockchain')
 const Block = require('../src/block')
 
 describe('Blockchain', () => {
-  const blockchain = new Blockchain()
+  let blockchain = new Blockchain()
 
   it('containas a `chain` Array of instance', () => {
     expect(blockchain.chain instanceof Array).toBe(true)
@@ -17,5 +17,43 @@ describe('Blockchain', () => {
     blockchain.addBlock({ data: newData })
     const lastBlockInChain = blockchain.chain[blockchain.chain.length - 1]
     expect(lastBlockInChain.data).toEqual(newData)
+  })
+
+  describe('isValidChain()', () => {
+    describe('when the chain does not start with the genesis block', () => {
+      it('returns false', () => {
+        blockchain.chain[0] = { data: 'fake block' }
+        expect(Blockchain.isValidChain(blockchain.chain).toBe(false))
+      })
+    })
+
+    describe('when the chain starts with the genesis block and has muliple blocks', () => {
+      beforeEach(() => {
+        blockchain = new Blockchain()
+        blockchain.addBlock({ data: 'a1' })
+        blockchain.addBlock({ data: 'a2' })
+        blockchain.addBlock({ data: 'a3' })
+      })
+
+      describe('and a lastHash reference has change', () => {
+        it('returns false', () => {
+          blockchain.chain[2].hash = 'fake hash'
+          expect(Blockchain.isValidChain(blockchain.chain).toBe(false))
+        })
+      })
+
+      describe('and the chain contains a block with an invalid field ', () => {
+        it('returns false', () => {
+          blockchain.chain[2].data = 'fake data'
+          expect(Blockchain.isValidChain(blockchain.chain).toBe(false))
+        })
+      })
+
+      describe('and the chain does not contain any invalid blocks ', () => {
+        it('returns true', () => {
+          expect(Blockchain.isValidChain(blockchain.chain).toBe(true))
+        })
+      })
+    })
   })
 })
