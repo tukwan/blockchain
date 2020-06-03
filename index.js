da96fad41e4a2cc958d00dd19f4667ae991ebab8
deployAt <nzbiegien@gmail.com>
2020-06-03 09:25:39 +0100

    feat: dev-peers & broadcast new chain on mine

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const Blockchain = require('./src/blockchain')
 const PubSub = require('./pubsub')
 
 const app = express()
-const PORT = 3000
 app.use(bodyParser.json())
+
 const blockchain = new Blockchain()
 const pubsub = new PubSub({ blockchain })
 
@@ -18,7 +18,16 @@ app.get('/api/blocks', (req, res) => res.json(blockchain.chain))
 app.post('/api/mine', (req, res) => {
   const { data } = req.body
   blockchain.addBlock({ data })
+  pubsub.broadcastChain()
   res.redirect('/api/blocks')
 })
 
+// dev-peer
+const DEFAULT_PORT = 3000
+let PEER_PORT
+if (process.env.GENERATE_PEER_PORT === 'true') {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000)
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`))
