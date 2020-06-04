6ac079be28faeb7cde31fc53dfd109b7c06bc033
deployAt <nick@deploy.at>
2020-06-04 20:44:49 +0100

    feat/test: tdd Wallet sign/verify data

--- src/wallet/__test__/wallet.app.test.js
+++ src/wallet/__test__/wallet.app.test.js
@@ -1,17 +1,42 @@
 const Wallet = require('../wallet.app')
+const { verifySignature } = require('../../utils/ec')
 
 describe('Wallet', () => {
   let wallet
 
   beforeEach(() => {
     wallet = new Wallet()
-  });
+  })
 
   it('has a `balance`', () => {
     expect(wallet).toHaveProperty('balance')
-  });
+  })
 
   it('has a `publicKey`', () => {
     expect(wallet).toHaveProperty('publicKey')
-  });
-});
+  })
+
+  describe('signing data', () => {
+    const data = 'abc'
+
+    it('verifies a signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: wallet.sign(data),
+        })
+      ).toBe(true)
+    })
+
+    it('does not verify an invalid signature', () => {
+      expect(
+        verifySignature({
+          publicKey: wallet.publicKey,
+          data,
+          signature: new Wallet().sign(data),
+        })
+      ).toBe(false)
+    })
+  })
+})
--- src/wallet/wallet.app.js
+++ src/wallet/wallet.app.js
@@ -1,12 +1,16 @@
 const { STARTING_BALANCE } = require('../config')
 const { ec } = require('../utils/ec')
+const cryptoHash = require('../utils/crypto-hash')
 
 class Wallet {
   constructor() {
     this.balance = STARTING_BALANCE
+    this.keyPair = ec.genKeyPair()
+    this.publicKey = this.keyPair.getPublic().encode('hex')
+  }
 
-    const keyPair = ec.genKeyPair()
-    this.publicKey = keyPair.getPublic().encode('hex')
+  sign(data) {
+    return this.keyPair.sign(cryptoHash(data))
   }
 }
 
