7169e8f625fae7743e0b62ae788f1b0d155e7549
deployAt <nick@deploy.at>
2020-06-05 07:31:17 +0100

    feat/test: tdd Transaction UTXO input

--- src/wallet/transaction.js
+++ src/wallet/transaction.js
@@ -4,6 +4,7 @@ class Transaction {
   constructor({ senderWallet, recipient, amount }) {
     this.id = uuid()
     this.outputMap = this.createOutputMap({ senderWallet, recipient, amount })
+    this.input = this.createInput({ senderWallet, outputMap: this.outputMap })
   }
 
   createOutputMap({ senderWallet, recipient, amount }) {
@@ -11,6 +12,15 @@ class Transaction {
     outputMap[recipient] = amount
     outputMap[senderWallet.publicKey] = senderWallet.balance - amount
     return outputMap
+  }
+
+  createInput({ senderWallet, outputMap }) {
+    return {
+      timestamp: Date.now(),
+      amount: senderWallet.balance,
+      address: senderWallet.publicKey,
+      signature: senderWallet.sign(outputMap)
+    }
   }
 }
 
