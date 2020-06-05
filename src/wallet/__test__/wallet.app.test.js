c333c23e01c68bea3558b51cfa8fd2fe11b9ab73
deployAt <nick@deploy.at>
2020-06-05 16:53:43 +0100

    feat/test: tdd Wallet createTransaction()

--- src/wallet/__test__/wallet.app.test.js
+++ src/wallet/__test__/wallet.app.test.js
@@ -1,4 +1,5 @@
 const Wallet = require('../wallet.app')
+const Transaction = require('../transaction')
 const { verifySignature } = require('../../utils')
 
 describe('Wallet', () => {
@@ -37,6 +38,38 @@ describe('Wallet', () => {
           signature: new Wallet().sign(data),
         })
       ).toBe(false)
+    })
+  })
+
+  describe('createTransaction()', () => {
+    describe('and the amount exceeds the balance', () => {
+      it('throws an error', () => {
+        expect(() =>
+          wallet.createTransaction({ amount: 999999, recipient: 'foo-recipient' })
+        ).toThrow('Amount exceeds balance')
+      })
+    })
+
+    describe('and the amount is valid', () => {
+      let transaction, amount, recipient
+
+      beforeEach(() => {
+        amount = 50
+        recipient = 'foo-recipient'
+        transaction = wallet.createTransaction({ amount, recipient })
+      })
+
+      it('creates and instance of `Transaction`', () => {
+        expect(transaction instanceof Transaction).toBe(true)
+      })
+
+      it('matches the transaction input with the wallet', () => {
+        expect(transaction.input.address).toEqual(wallet.publicKey)
+      })
+
+      it('outputs the amount the recipient', () => {
+        expect(transaction.outputMap[recipient]).toEqual(amount)
+      })
     })
   })
 })
